9aaef3c9f1bb899697f05d4adfbc82c290858881
“Pedro <padronpedro@gmail.com>
2022-03-01 17:20:04 -0500

    adding mix to app css/js

--- resources/views/layouts/vue.blade.php
+++ resources/views/layouts/vue.blade.php
@@ -9,13 +9,14 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body class="antialiased">
 
     <div id="app"></div>
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
