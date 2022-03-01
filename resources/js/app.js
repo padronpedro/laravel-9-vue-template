03b63e709210b9855de4b8cc63f61a23f0bbef11
“Pedro <padronpedro@gmail.com>
2022-03-01 17:19:27 -0500

    added vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,14 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require('./bootstrap')
 
-window.Vue = require('vue').default;
+window.Vue = require('vue').default
 
-import router from './router';
-import App from './layouts/App.vue';
+import router from './router'
+import store from './store'
+import App from './layouts/App.vue'
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -21,7 +23,7 @@ import App from './layouts/App.vue';
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -31,6 +33,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     router,
+    store,
     el: '#app',
     render: h => h(App)
 });
